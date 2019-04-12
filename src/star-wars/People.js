cfdae2f93703257dbe7083a71bdec12c7fb67cd5
Matthew Wratt <mw@trineo.com>
2019-04-12 16:39:36 +1200

    Load and display Star Wars people

--- src/star-wars/People.js
+++ src/star-wars/People.js
@@ -2,11 +2,76 @@ import React from 'react'
 import './People.css'
 
 export default class People extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      loading: true,
+      people: [],
+    }
+  }
+
+  componentDidMount() {
+    this.mounted = true
+    this.fetchPeople()
+  }
+
+  componentWillUnmount() {
+    this.mounted = false
+  }
+
+  async fetchPeople() {
+    let people = []
+    let error = null
+
+    this.setState({ people, error, loading: true })
+
+    try {
+      const response = await fetch('https://swapi.co/api/people/')
+      if (!response.ok) {
+        new Error('Unable to fetch people')
+      }
+
+      const body = await response.json()
+
+      people = body.results
+    } catch (err) {
+      error = err
+    }
+
+    if (!this.mounted) {
+      return
+    }
+
+    this.setState({ people, error, loading: false })
+  }
+
   render() {
+    const { loading, people, error } = this.state
+
     return (
       <div className='StarWarsPeople' data-testid='star-wars-people-container'>
         <h1 data-testid='header'>Star Wars People</h1>
-        <p data-testid='loading-indicator'>Loading...</p>
+
+        {loading && (
+          <p className='loading' data-testid='loading-indicator'>
+            Loading...
+          </p>
+        )}
+
+        {!loading && !error && (
+          <ul data-testid='people-list'>
+            {people.map(({ url, name }) => (
+              <li key={url}>{name}</li>
+            ))}
+          </ul>
+        )}
+
+        {error && (
+          <pre className='error' data-testid='error'>
+            {error.toString()}
+          </pre>
+        )}
       </div>
     )
   }
--- src/star-wars/People.test.js
+++ src/star-wars/People.test.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import { render, cleanup } from 'react-testing-library'
+import { render, waitForElement } from 'react-testing-library'
 import 'jest-dom/extend-expect'
+import peopleFixture from './__fixtures__/people'
 import People from './People'
 
 describe('start-wars/People', () => {
+  beforeEach(() => {
+    fetch.mockResponse(JSON.stringify(peopleFixture))
+  })
+
   it('has a title', () => {
     const { getByTestId } = render(<People />)
     expect(getByTestId('header')).toHaveTextContent('Star Wars People')
@@ -12,5 +17,17 @@ describe('start-wars/People', () => {
   it('is loading', () => {
     const { getByTestId } = render(<People />)
     expect(getByTestId('loading-indicator')).toHaveTextContent('Loading...')
+  })
+
+  it('shows a list of people', async () => {
+    const { getByTestId, queryByTestId } = render(<People />)
+    expect(getByTestId('loading-indicator')).toHaveTextContent('Loading...')
+
+    await waitForElement(() => getByTestId('people-list'))
+
+    expect(queryByTestId('loading-indicator')).toBe(null)
+    expect(getByTestId('people-list')).toHaveTextContent(
+      peopleFixture.results.map(({ name }) => name).join(''),
+    )
   })
 })
